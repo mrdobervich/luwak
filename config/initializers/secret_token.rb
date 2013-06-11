86547c06e9887f63a3ad2fb3f17d6cca0ad55ac8
David <david@ubuntu.(none)>
2013-06-11 12:02:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luwak::Application.config.secret_token = '12d76bea4784ca688129c41fd08e64aee2d1695db5c0a246648cc74d1e7e8fc847d7241a266069483676ae38ba154393f1e9d8e77d2a9befef6d668cdc2959f4'
